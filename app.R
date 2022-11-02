995aa90b801da0a9fc86e66cbf383c5477081fb6
ppaxisa <pp.axisa@gmail.com>
2022-11-02 16:41:26 +0100

    first commit

---
+++ app.R
@@ -0,0 +1,5 @@
+# Libraries
+if(!require(BiocManager)) install.packages("BiocManager")
+if(!require(tidyverse)) install.packages("tidyverse")
+if(!require(iSEE)) BiocManager::install("iSEE")
+if(!require(iSEEu)) BiocManager::install("iSEEu")
